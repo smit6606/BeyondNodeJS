c79f0dc9025387299916d7999e716f43e52734a3
smit6606 <smeetgarala6606@gmail.com>
2025-02-24 22:42:35 +0530

    Calculator  (24 February 2025)
    Switch-Case-Calculator in NodeJs

---
+++ Calculator/index.js
@@ -0,0 +1,7 @@
+let calc = require("./math");
+
+let num1 = 20;
+let num2 = 10;
+let operator = "+";
+
+console.log(`Result: ${calc(num1, num2, operator)}`);
---
+++ Calculator/math.js
@@ -0,0 +1,16 @@
+function calc(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+    default:
+      return "Invalid operator";
+  }
+}
+
+module.exports = calc;
